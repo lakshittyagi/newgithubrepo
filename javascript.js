8dc7355b9f66744ee451a07d8a083dd8edf6d350
lakshit <akshutyagi2016@gmail.com>
2020-08-30 14:53:52 +0530

    updated javascript.js in dev enviornment

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var name = "akshu tyagi";
+
+console.log(name);
+
+myfun = function() {
+	for(var i = 0; i<10; i++){
+	 	document.getElementById('new').innerHTML = name;
+	}
+
+var lastName ="Tyagi";
+console.log(lastName);
+		
+}
