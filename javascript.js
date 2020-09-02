da8df69fa39bb87a576e55b323ff5780d74b336a
lakshit <akshutyagi2016@gmail.com>
2020-09-02 17:17:34 +0530

    game logic version 0.0.1

--- javascript.js
+++ javascript.js
@@ -1,13 +1,33 @@
-var name = "akshu tyagi";
+var dice , score , activePlayer , roundScore;
+activePlayer = 0;
+score = [0,0];
+roundScore  = 0;
+ 
+function myfun() {
+	var dice = Math.floor(Math.random() * 6) + 1;
+	document.querySelector('#dice').textContent = dice;
+	if (dice !== 1) {
+		roundScore += dice; 
+		document.getElementById('current-' + activePlayer).textContent = roundScore;
 
-console.log(name);
-var lastName ="Tyagi";
-myfun = function() {
+	}
+	else {
+		roundScore = 0;
+		document.getElementById('current-' + activePlayer).textContent = roundScore;
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		
+	}
 
-	 	document.getElementById('new').innerHTML = name;
-	 	document.getElementById('name').innerHTML = lastName;
 }
 
-console.log(lastName);
+function hold() {
+   
+	   score[activePlayer] +=  roundScore;
+	   document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+	   roundScore = 0;
+	   document.getElementById('current-' + activePlayer).textContent = roundScore;
+	   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+   
 
-console.log('hello world');	
+}
+
